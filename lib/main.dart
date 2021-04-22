0d1dac79fb82a5304ea09af555e2813ac29f0e0a
ReethuSanagala <reethusanagala@gmail.com>
2021-04-23 00:35:53 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,21 @@ void main() {
   runApp(MyApp());
 }
 
-class  MyApp extends  StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(backgroundColor:Colors.pink,
-      body: Container( 
-        color:Colors.white,
-        height: 20.0,
-        width: 150.0,
-        margin: EdgeInsets.only(left: 30.0,top: 100.0),
-        child: Text('My Name is Reethu'),
-      ), 
-     
-      )
-    );
+        home: Scaffold(
+      backgroundColor: Colors.pink,
+      body: Coloumn(Children: <Widget>[
+        Container(
+          color: Colors.white,
+          height: 20.0,
+          width: 150.0,
+          margin: EdgeInsets.only(left: 30.0, top: 100.0),
+          child: Text('My Name is Reethu'),
+        ),
+      ]),
+    ));
   }
 }
-
-
